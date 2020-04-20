385c859197067858a5e06cce332f1895fa3d8c25
Marcílio Cavalcanti <m.matos.cavalcanti@avanade.com>
2020-04-20 00:41:10 -0300

    Lista de usuarios no endpoint /nova

--- src/main/java/br/com/softblue/bluewitter/controller/MensagemController.java
+++ src/main/java/br/com/softblue/bluewitter/controller/MensagemController.java
@@ -1,8 +1,10 @@
 package br.com.softblue.bluewitter.controller;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.softblue.bluewitter.domain.mensagem.Mensagem;
 import br.com.softblue.bluewitter.domain.mensagem.MensagemRepository;
+import br.com.softblue.bluewitter.domain.usuario.Usuario;
+import br.com.softblue.bluewitter.domain.usuario.UsuarioRepository;
 
 @Controller
 @RequestMapping("/mensagem")
@@ -19,10 +23,17 @@ public class MensagemController {
 	@Autowired
 	private MensagemRepository mensagemRepository;
 	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
 	@GetMapping("/nova")
 	public String nova(Model model) {
 		Mensagem mensagem = new Mensagem();
 		model.addAttribute("mensagem", mensagem);
+	
+		List<Usuario> usuarios = usuarioRepository.findAll(Sort.by(Sort.Direction.ASC, "nome"));
+		model.addAttribute("usuarios", usuarios);
+		
 		return "create-message";
 	}
 	
